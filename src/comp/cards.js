59916058a6355ab57576d1eb54d7fa87e365c568
Holappa NiinaE <niina.p.puurunen@gmail.com>
2022-02-20 19:54:25 +0200

    kommentit lisätty

--- src/comp/cards.js
+++ src/comp/cards.js
@@ -3,9 +3,12 @@ import { Card, Button } from 'semantic-ui-react';
 import axios from 'axios';
 
 export default function Cards() {
+    // tilamuuttuja
     const [items, setItems] = useState([]);
+    // globaali muuttuja
     const URL = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
 
+    // hakee tiedot apista
     const fetchQuotes = () => {
         axios.get(URL)
         .then((response) => {
@@ -18,6 +21,7 @@ export default function Cards() {
         <img src='/img/Breakingbad.jpg' />
         <Card className='card'>
             <h3>Breaking Bad quotes</h3>
+            {/* käydään itemit läpi */}
             {items.map((data) =>{
                 return (
                 <div>
@@ -26,7 +30,7 @@ export default function Cards() {
                 </div>
                 )
                 })}
-
+            {/* nappia painamalla kutsutaan fetchQuotes funktiota ja käyttöliittymään tulee quote näkyviin */}
             <Button onClick={fetchQuotes}>Click for a quote</Button>
         </Card>
     </div>
